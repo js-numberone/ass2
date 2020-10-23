6bfbf6b756ad8b95b9363a7e8745294d2d1454f2
js-numberone <56318658+js-numberone@users.noreply.github.com>
2020-10-23 14:28:44 -0400

    Add files via upload

---
+++ A5/MainWindow.xaml.cs
@@ -0,0 +1,210 @@
+﻿/*
+*	FILE			: MainWindow.xaml.cs
+*	PROJECT			: PROG2121 - Assignment 5
+*	PROGRAMMER		: John Stanley and Aaron Perry
+*	FIRST VERSION	: 2019-11-01
+*	DESCRIPTION		: This file holds the UI and events handlers for the client interface.  Messages are sent via NetworkStream and TcpCLient streams.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace A5
+{
+    /* -------------------------------------------------------------
+NAME	:	MainWindow
+PURPOSE :	This class creates the main window class
+
+------------------------------------------------------------- */
+    public partial class MainWindow : Window
+    {
+        TcpClient clientSocket = new TcpClient();
+        NetworkStream serverStream = default(NetworkStream);
+        List<String> msgList = new List<String>();
+        List<String> userName = new List<String>();
+        static string readData = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            connect_btn.IsEnabled = false;
+            userListbox.IsEnabled = false;
+        }
+
+
+        /*
+                Name	:   Connect_btn_Click
+                Purpose :   Handles the events of the connect buttons click.  This sends the connection request via TcpClient stream and listens for server messages via a thread poll
+                Inputs	:	object sender, RoutedEventArgs e
+                Outputs	:	NONE
+                Returns	:	Nothing
+        */
+
+        private void Connect_btn_Click(object sender, RoutedEventArgs e)
+        {
+            clientSocket.Connect(ipTextBox.Text, Int32.Parse(portTextBox.Text));
+            nameTextBox.IsEnabled = false;
+            connect_btn.IsEnabled = false;
+            ipTextBox.IsEnabled = false;
+            portTextBox.IsEnabled = false;
+            
+            userName.Add(nameTextBox.Text);
+
+            //Get StreamWriter Stream
+            StreamWriter sW = new StreamWriter(clientSocket.GetStream());
+            sW.AutoFlush = true;
+
+            // Send the username
+            sW.WriteLine(nameTextBox.Text);
+
+            StreamReader sR = new StreamReader(clientSocket.GetStream());
+
+            //Waits to see SyreamReader has any available data to be read 
+            while(sR.Peek() != -1)
+            {
+                 // Read the username (waiting for the client to use WriteLine())
+                string connectedUsername = sR.ReadLine();
+                if(userName.Contains(connectedUsername))
+                userName.Add(connectedUsername);
+                userListbox.Items.Add(connectedUsername);
+            }
+           
+
+
+            //Start Thread
+            Thread ctThread = new Thread(getMessage);
+            ctThread.Start();
+            
+        }
+
+
+        /*
+                Name	:   getMessage
+                Purpose :   This function handles the listening POLL for messages from the server
+                Inputs	:	NONE
+                Outputs	:	NONE
+                Returns	:	Nothing
+        */
+        private void getMessage()
+        {
+
+            string returnData;
+
+            while(true)
+            {
+                serverStream = clientSocket.GetStream();
+                
+                byte[] instream = new byte[256];
+
+                serverStream.Read(instream, 0, instream.Length);
+
+                returnData = System.Text.Encoding.ASCII.GetString(instream);
+
+                readData = returnData;
+
+                msgList.Add(readData);
+
+                messageCheck();
+                
+            }
+        }
+
+        /*
+                Name	:   messageCheck
+                Purpose :   This function checks for thread access and if no access Invokes the thread and else adds message to the message box
+                Inputs	:	NONE
+                Outputs	:	Message in textbox
+                Returns	:	Nothing
+        */
+
+        private void messageCheck()
+        {
+            if(!Dispatcher.CheckAccess())
+            {
+                Dispatcher.Invoke(messageCheck);
+            }
+            else
+            {
+                inMessageBox.Text = "";
+                for (int i=0;i<msgList.Count;i++)
+                {
+                 inMessageBox.AppendText(msgList[i] + "\n");
+                
+                }
+            }
+        }
+
+        /*
+               Name	    :   Msg_btn_Click
+               Purpose  :   This function encodes the message and prpares it for transmission over TCP/IP.
+               Inputs	:	object sender, RoutedEventArgs e
+               Outputs	:	NONE
+               Returns	:	Nothing
+       */
+
+        private void Msg_btn_Click(object sender, RoutedEventArgs e)
+        {
+            string outGoing = outMessageBox.Text;
+        
+            byte[] msgStream = Encoding.ASCII.GetBytes(outGoing);
+            
+
+            serverStream.Write(msgStream, 0, msgStream.Length);
+            serverStream.Flush();
+            outMessageBox.Text = "";
+
+        }
+        /*
+               Name	    :   nameTextBox_TextChanged
+               Purpose  :   Handles the event of textbox change state.  If the text box is populated, the connect button is enabled allowing connection to the server
+               Inputs	:	object sender, RoutedEventArgs e
+               Outputs	:	NONE
+               Returns	:	Nothing
+       */
+
+        private void nameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+
+                if (nameTextBox.Text.Length > 0 )
+                    connect_btn.IsEnabled = true;
+                else
+                    connect_btn.IsEnabled = false;
+
+        }
+
+        /*
+              Name	    :   disconnect_btn_Click
+              Purpose   :   Handles the event of the dicsonnct button.  When fired the user name in the text box is send along with a stream flush allowing for a gracefull stream close()
+              Inputs	:	object sender, RoutedEventArgs e
+              Outputs	:	NONE
+              Returns	:	Nothing
+      */
+        private void disconnect_btn_Click(object sender, RoutedEventArgs e)
+        {
+            string outGoing = nameTextBox.Text;
+
+            byte[] msgStream = Encoding.ASCII.GetBytes(outGoing);
+
+
+            serverStream.Write(msgStream, 0, msgStream.Length);
+            serverStream.Flush();
+            disconnect_btn.IsEnabled = false;
+      
+        }
+    }
+}
---
+++ Client/Server.cs
@@ -0,0 +1,207 @@
+﻿/*
+*	FILE			: Server.cs
+*	PROJECT			: PROG2121 - Assignment 5
+*	PROGRAMMER		: John Stanley and Aaron Perry
+*	FIRST VERSION	: 2019-11-01
+*	DESCRIPTION		: This file holds the class for the which instantiates the server logic.  The server Logic is controlled by a worker function through a thread.  The messages are recieved and sent via
+*	                a TCP/IP Listener and TcpClient.  The usernames and discnonnects are sent via StreamReader and StreamWriter
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace A5
+{
+    /* -------------------------------------------------------------
+NAME	:	Server
+PURPOSE :	This class creates the main Server object
+
+------------------------------------------------------------- */
+
+    class Server
+    {
+        NetworkStream stream = new NetworkStream(default);
+        static Dictionary<string, NetworkStream> clientStreams = new Dictionary<string, NetworkStream>();
+        static List<string> connectedUsers = new List<string>();
+
+        static int userID = 0 ;
+        static void Main(string[] args)
+        {
+
+
+            TcpListener server = null;
+            try
+            {
+                // Set the TcpListener on port 13000.
+                Int32 port = 13000;
+                IPAddress localAddr = IPAddress.Parse("10.192.49.5");
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for client requests.
+                server.Start();
+               
+
+                // Enter the listening loop.
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    // Perform a blocking call to accept requests.
+                    // You could also user server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    StreamReader sR = new StreamReader(client.GetStream());
+
+                    // Read the username (waiting for the client to use WriteLine())
+                    String connectedUsername = sR.ReadLine();
+                    connectedUsers.Add(connectedUsername);
+                    NetworkStream stream = client.GetStream();
+                    
+                    
+                    Console.WriteLine(connectedUsername + " is connected");
+
+
+                    StreamWriter sW = new StreamWriter(client.GetStream());
+                    
+                    //Check for existing clients
+                    if(clientStreams.Count == 0)
+                    {
+                        sW.WriteLine(connectedUsers[0]);
+                        // Send the username
+                        sW.AutoFlush = true;
+                        clientStreams.Add(connectedUsername, stream);
+                    }
+                    //Send username to all clients
+                    else
+                    {
+                        clientStreams.Add(connectedUsername, stream);
+                        int i = 0;
+                        foreach (KeyValuePair<string, NetworkStream> user in clientStreams)
+                        {
+                            
+                            sW.WriteLine(connectedUsers[i]);
+                            i++;
+                            // Send the username
+                            
+                        }
+                    }
+                    sW.AutoFlush = true;
+
+
+
+                    ParameterizedThreadStart ts = new ParameterizedThreadStart(Worker);
+                    Thread clientThread = new Thread(ts);
+                    clientThread.Start(client);
+
+
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+        }
+
+/*
+Name	: Worker
+Purpose : Creates the worker thread to handle clients streams
+Inputs	:	Object thread
+Outputs	:	NONE
+Returns	:	Nothing
+*/
+        public static void Worker(Object o)
+        {
+
+            TcpClient client = (TcpClient)o;
+            // Buffer for reading data
+            Byte[] bytes = new Byte[256];
+            String data = null;
+
+           
+
+            // Get a stream object for reading and writing
+            NetworkStream stream = client.GetStream();
+
+            
+
+            int i;
+           // clientStreams.Add(connectedUsers[userID], stream);
+            userID++;
+            // Loop to receive all the data sent by the client.
+            while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+            { 
+           
+                // Translate data bytes to a ASCII string.
+                data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                
+                //Check for disconnected clients
+                if(connectedUsers.Contains(data))
+                {
+
+                    Console.WriteLine("{0} is disconnected", data);
+                    clientStreams.Remove(data);
+                    byte[] Closemsg = System.Text.Encoding.ASCII.GetBytes(data + " is disconnected");
+                    foreach (KeyValuePair<string, NetworkStream> user in clientStreams)
+                    {
+
+                        // Send back a response.
+                        user.Value.Write(Closemsg, 0, Closemsg.Length);
+                        
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Received: {0}", data);
+                    // Process the data sent by the client.
+                    data = data.ToUpper();
+
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+
+                    //Sends messages back to all clients connected
+                    foreach (KeyValuePair<string, NetworkStream> user in clientStreams)
+                    {
+
+
+                        
+                        user.Value.Write(msg, 0, msg.Length);
+                        Console.WriteLine("Sent: {0}", data);
+
+                    }
+                }
+             
+               
+
+            }
+
+          
+            // Shutdown and end connection
+
+            client.Close();
+            
+
+        }
+
+    }
+}
+
